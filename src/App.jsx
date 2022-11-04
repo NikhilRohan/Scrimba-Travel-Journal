1c803e7d7046699963f81303243ae97c2cbe29cf
NikhilRohan <nikhilrohan0108@gmail.com>
2022-11-05 02:29:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>Travel Journal
+    </div>
+  )
+}
+
+export default App
